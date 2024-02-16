81c83911fb71a17d9042649ff3350d2eae6e4bd1
narendrarty17 <narendrarty17@gmail.com>
2024-02-16 04:36:42 -0800

    added latest article section

--- pages/careerhome.js
+++ pages/careerhome.js
@@ -1,3 +1,4 @@
+import OverlappingCard from '@/components/Blogs/homeComponents/subHomeComponents/OverlappingCard';
 import Footer from '@/components/utils/Footer';
 import Header from '@/components/utils/Header';
 import React from 'react'
@@ -20,6 +21,20 @@ export const Career = () => {
             Read me
           </button>
         </div>
+      </div>
+      <div className="px-6 md:px-8 lg:px-20 flex flex-col itemx-center gap-8">
+        <div className="flex gap-4 justify-center md:justify-start">
+          <span className="bg-green-400 w-8 h-8 rounded-full" />
+          <h1 className="text-2xl">Learning</h1>
+        </div>
+        <div className="flex flex-wrap flex-col gap-10 mr-[10px] items-center md:justify-between md:flex-row">
+          <OverlappingCard id={1} type="learning" />
+          <OverlappingCard id={1} type="learning" />
+          <OverlappingCard id={1} type="learning" />
+        </div>
+        <button className="mx-auto text-lg border-2 border-white text-white py-3 px-6 rounded-full">
+          See More
+        </button>
       </div>
       <Footer />
     </div>
